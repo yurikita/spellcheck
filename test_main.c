7c005467a3d40aff92396df3151fffb2621454fd
yuyu29 <55943282+yuyu29@users.noreply.github.com>
2019-09-29 16:18:31 -0400

    Create test_main.c

---
+++ test_main.c
@@ -0,0 +1,16 @@
+#include <check.h>
+#include "dictionary.h"
+#include <stdlib.h>
+
+#define DICTIONARY "wordlist.txt"
+#define TESTDICT "test_worlist.txt"
+
+START_TEST(test_dictionary_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    ck_assert(load_dictionary(TESTDICT, hashtable));
+    // Here we can test if certain words ended up in certain buckets
+    // to ensure that our load_dictionary works as intended. I leave
+    // this as an exercise.
+}
+END_TEST
